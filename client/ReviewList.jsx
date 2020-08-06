6c246e77de7175f9019bb7c5de91ebf4324bd246
James Olivas <jamesbolivas@gmail.com>
2020-08-05 17:19:23 -0700

    Prepares client for (and begins to write) rudimentary ReviewList component

---
+++ client/ReviewList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ReviewList extends React.Component {
+  render() {
+    return (
+      <div>hello i am a review list</div>
+    );
+  }
+}
+
+export default ReviewList;
--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReviewList from './ReviewList.jsx';
 
 class App extends React.Component {
   render() {
     return(
-      <div>Hello, world!</div>
+      <div>
+        <div>Hello, world!</div>
+        <ReviewList/>
+      </div>
     );
   }
 }
