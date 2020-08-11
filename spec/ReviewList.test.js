7830a5b1169ffff53b04f002a2fac3b51412efac
James Olivas <jamesbolivas@gmail.com>
2020-08-10 17:32:51 -0700

    Fleshes out test suite a little bit

--- spec/ReviewList.test.js
+++ spec/ReviewList.test.js
@@ -1,12 +1,30 @@
-import { mount, shallow, configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
+import ReviewList from '../client/ReviewList.jsx';
 
 configure({ adapter: new Adapter() });
 
-import ReviewList from './../client/ReviewList.jsx';
+describe('ReviewList', () => {
+  it('renders correctly', () => {
+    shallow(<ReviewList />);
+  });
 
-test('<ReviewList />', () => {
-  const wrapper = mount(<ReviewList />);
-  expect(wrapper.find('div')).toExist();
+  it('has a properly formatted state component', () => {
+    const wrapper = shallow(<ReviewList />);
+    expect(wrapper.instance().state.currentItem).toEqual({});
+    expect(wrapper.instance().state.reviews).toEqual([]);
+    expect(wrapper.instance().state.rating).toEqual(0);
+    expect(wrapper.instance().state.isShowingReviews).toEqual(false);
+  });
+
+  it('changes isShowingReviews state when toggleReadMore is called', () => {
+    const wrapper = shallow(<ReviewList />);
+    const instance = wrapper.instance();
+    expect(instance.state.isShowingReviews).toEqual(false);
+    instance.toggleReadMore();
+    expect(instance.state.isShowingReviews).toEqual(true);
+    instance.toggleReadMore();
+    expect(instance.state.isShowingReviews).toEqual(false);
+  });
 });
