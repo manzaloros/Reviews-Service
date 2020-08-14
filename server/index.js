60f4d556c6bcdc7dfd431fb6696847d06c601c07
James Olivas <jamesbolivas@gmail.com>
2020-08-14 12:43:54 -0700

    Fixes problem that caused db to crash when giving bad search

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const port = 2625;
 
 app.use('/dist', express.static(path.join(__dirname, './../dist')));
 app.use('/item/:user', express.static(path.join(__dirname, './../public')));
-app.use('/', function(req, res, next) {
+app.use('/', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
@@ -24,31 +24,51 @@ app.get('/api/seller', (req, res) => {
 
 app.get('/api/seller/:id', (req, res) => {
   db.getOneSeller(req.params.id, (data) => {
-    res.send(data);
+    if (data === '404') {
+      res.sendStatus(404);
+    } else {
+      res.send(data);
+    }
   });
 });
 
 app.get('/api/item', (req, res) => {
   db.getAllListings((data) => {
-    res.send(data);
+    if (data === '404') {
+      res.sendStatus(404);
+    } else {
+      res.send(data);
+    }
   });
 });
 
 app.get('/api/item/endpoint/:listingId', (req, res) => {
   db.getOneListingByEndpoint(req.params.listingId, (data) => {
-    res.send(data);
+    if (data === '404') {
+      res.sendStatus(404);
+    } else {
+      res.send(data);
+    }
   });
 });
 
 app.get('/api/item/:listingId', (req, res) => {
   db.getOneListing(req.params.listingId, (data) => {
-    res.send(data);
+    if (data === '404') {
+      res.sendStatus(404);
+    } else {
+      res.send(data);
+    }
   });
 });
 
 app.get('/api/item/:listingId/reviews', (req, res) => {
   db.getSellerReviewsForListing(req.params.listingId, (data) => {
-    res.send(data);
+    if (data === '404') {
+      res.sendStatus(404);
+    } else {
+      res.send(data);
+    }
   });
 });
 
