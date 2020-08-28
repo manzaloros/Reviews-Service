dfea8076a8dfb31aee18e149f5dce19af29292ed
manzaloros <zacharymansellcello@gmail.com>
2020-08-27 17:46:19 -0700

    Add colors, add limit variable for iterations

--- database/dataGenerationScript.js
+++ database/dataGenerationScript.js
@@ -2,8 +2,11 @@
 const fs = require('fs');
 const faker = require('faker');
 const now = require('performance-now');
+const colors = require('colors');
+// Data Size:
+const limit = 10000000;
 
-const generateData = () => {
+const generateData = (dataSize) => {
   let data = '';
   let _id = 0;
   let review = {
@@ -15,7 +18,7 @@ const generateData = () => {
     id: faker.random.number({ min: 0, max: 100 }),
     description: faker.lorem.paragraphs(2),
   };
-  for (let i = 0; i < 100; i += 1) {
+  for (let i = 0; i < dataSize; i += 1) {
     // Reassign _id so that guitar and review are linked
     _id = i;
     if (i % 5 === 0) {
@@ -43,19 +46,20 @@ const generateData = () => {
     if (err) {
       throw err;
     }
-    console.log('File Cleared');
+    console.log('File Cleared!'.blue);
     fs.writeFile('database/seedFiles/test.txt', data, (error) => {
       if (error) {
         throw error;
       }
-      const stats = fs.statSync['database/seedFiles/test.txt'];
+      const stats = fs.statSync('database/seedFiles/test.txt');
       const fileSizeInBytes = stats.size;
-      console.log(`The file has been saved! File size is ${fileSizeInBytes}`);
+      console.log(`The file has been saved! File size is ${fileSizeInBytes / 1000000.0} MBs`.red);
     });
   });
 };
 
-const start = performance.now();
-generateData();
-const end = performance.now();
-console.log(`generateData() took ${start - end} milliseconds`);
+// Track performance:
+const start = now();
+generateData(limit);
+const end = now();
+console.log(`generateData() took ${end - start} milliseconds`.green);
