e1ece418333313e0b2a0c40bc3ca3bd52750edd0
manzaloros <zacharymansellcello@gmail.com>
2020-08-29 15:48:00 -0700

    Add client disconnect to guitar seed

--- database/copyReviews.js
+++ database/copyReviews.js
@@ -2,19 +2,6 @@ const copyFrom = require('pg-copy-streams').from;
 const fs = require('fs');
 const db = require('./guitarSchema.js');
 
-// (async () => {
-//   const query = `\\COPY "Guitars"(name, "productId", id)
-//                 FROM './seedFiles/guitars.csv'
-//                 DELIMITER ','
-//                 CSV HEADER`;
-//   try {
-//     const [results] = await db.sequelize.query(query);
-//     return results;
-//   } catch (e) {
-//     return console.log(e);
-//   }
-// })();
-
 const copyGuitars = () => new Promise((resolve, reject) => {
   const stmt = `COPY "Guitars"(name, "productId", id)
                  FROM STDIN
@@ -30,26 +17,9 @@ const copyGuitars = () => new Promise((resolve, reject) => {
     fileStream.pipe(stream);
     stream.on('finish', () => {
       // returns promise
-      db.sequelize.connectionManager.releaseConnection();
+      db.sequelize.connectionManager.disconnect(client);
     });
   });
 });
 
 copyGuitars();
-
-// const { Pool } = require('pg');
-// const copyFrom = require('pg-copy-streams').from;
-
-// const pool = new Pool({
-//   host: 'localhost',
-//   database: 'reviews',
-//   port: '5432',
-// });
-
-// pool.connect((err, client, done) => {
-//   const stream = client.query(copyFrom('COPY "Guitars" FROM STDIN'));
-//   const fileStream = fs.createReadStream('./seedFiles/guitars.csv');
-//   fileStream.on('error', done);
-//   stream.on('finish', done);
-//   fileStream.pipe(stream);
-// });
