19f711d9b4f652cd405c9cfd7a77ba5e892f2a02
manzaloros <zacharymansellcello@gmail.com>
2020-08-31 17:23:22 -0700

    Remove ReviewModel and add to GuitarModel

--- database/GuitarModel.js
+++ database/GuitarModel.js
@@ -1,7 +1,17 @@
+/*
+ *  Guitar Model for Cassandra
+ *  Not using guitarId for reviews object since it is nested in a guitar
+ */
 module.exports = {
   fields: {
     name: 'text',
     productId: 'int',
+    reviews: {
+      rating: 'int',
+      author: 'string',
+      date: 'string',
+      description: 'string',
+    },
   },
   key: ['productId'],
 };
