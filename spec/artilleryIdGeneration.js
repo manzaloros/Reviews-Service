87d75e82ef767f2b57a12a60f39b673a203f6ed8
manzaloros <zacharymansellcello@gmail.com>
2020-09-03 18:42:36 -0700

    Change random number function to insert to yaml

--- spec/artilleryIdGeneration.js
+++ spec/artilleryIdGeneration.js
@@ -1,26 +1,11 @@
-const fs = require('fs');
-const path = require('path');
 const faker = require('faker');
 
-const writeStream = fs.createWriteStream(path.resolve(__dirname, 'artilleryIds.csv'));
-const generateFakeIds = (l, writer) => {
-  let i = l;
-  const write = () => {
-    let ok = true;
-    do {
-      const randomId = String(faker.random.number({ min: 1, max: 1e7 }));
-      i -= 1;
-      if (i === 0) {
-        writer.write(`${randomId}`);
-      } else {
-        ok = writer.write(`${randomId},`);
-      }
-    } while (i > 0 && ok);
-    if (i > 0) {
-      writer.once('drain', write);
-    }
-  };
-  write();
+const generateRandomId = (userContext, events, done) => {
+  const randomId = String(faker.random.number({ min: 1, max: 1e7 }));
+  userContext.vars.id = randomId;
+  return done();
 };
 
-generateFakeIds(1e7, writeStream);
+module.exports = {
+  generateRandomId,
+};
